022f41f4b50dcdce923a2d31e6c5a33acffde823
Mohitpatel021 <mohitpa021@gmail.com>
2024-11-03 19:40:01 +0530

    Return approved api is done

--- network/src/main/java/com/social/network/book/BookController.java
+++ network/src/main/java/com/social/network/book/BookController.java
@@ -26,46 +26,44 @@ public class BookController {
     public ResponseEntity<?> findAllBooks(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size, Authentication connectedUser) {
         return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
+
     @GetMapping("/owner")
-    public ResponseEntity<?> findAllBooksByOwner(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size, Authentication connectedUser)
-    {
+    public ResponseEntity<?> findAllBooksByOwner(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size, Authentication connectedUser) {
         return ResponseEntity.ok(service.findAllBooksByOwner(page, size, connectedUser));
     }
 
     @GetMapping("/borrowed")
-    public ResponseEntity<?> findAllBorrowedBooks(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size, Authentication connectedUser)
-    {
+    public ResponseEntity<?> findAllBorrowedBooks(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size, Authentication connectedUser) {
         return ResponseEntity.ok(service.findAllBorrowedBooks(page, size, connectedUser));
     }
 
     @GetMapping("/returned")
-    public ResponseEntity<?> findAllReturnedBooks(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size, Authentication connectedUser)
-    {
+    public ResponseEntity<?> findAllReturnedBooks(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size, Authentication connectedUser) {
         return ResponseEntity.ok(service.findAllReturnedBooks(page, size, connectedUser));
     }
 
     @PatchMapping("/shareable/{book-id}")
-    public ResponseEntity<?> updateShareableStatus(@PathVariable("book-id")Integer bookId, Authentication connectedUser) {
+    public ResponseEntity<?> updateShareableStatus(@PathVariable("book-id") Integer bookId, Authentication connectedUser) {
         return ResponseEntity.ok(service.updateShareableStatus(bookId, connectedUser));
     }
 
     @PatchMapping("/archived/{book-id}")
-    public ResponseEntity<?> updateArchivedStatus(@PathVariable("book-id")Integer bookId, Authentication connectedUser) {
+    public ResponseEntity<?> updateArchivedStatus(@PathVariable("book-id") Integer bookId, Authentication connectedUser) {
         return ResponseEntity.ok(service.updateArchivedStatus(bookId, connectedUser));
     }
+
     @PostMapping("/borrow/{book-id}")
-    public ResponseEntity<?> borrowBook(@PathVariable("book-id") Integer bookId, Authentication connectedUser){
+    public ResponseEntity<?> borrowBook(@PathVariable("book-id") Integer bookId, Authentication connectedUser) {
         return ResponseEntity.ok(service.borrowBook(bookId, connectedUser));
     }
+
     @PatchMapping("/borrow/return/{book-id}")
-    public ResponseEntity<?> returnBorrowBook(@PathVariable("book-id")Integer bookId, Authentication connectedUser) {
+    public ResponseEntity<?> returnBorrowBook(@PathVariable("book-id") Integer bookId, Authentication connectedUser) {
         return ResponseEntity.ok(service.returnBorrowBook(bookId, connectedUser));
     }
 
     @PatchMapping("/borrow/return/approve/{book-id}")
-    public ResponseEntity<?> approveReturnBorrowedBook(@PathVariable("book-id")Integer bookId, Authentication connectedUser) {
+    public ResponseEntity<?> approveReturnBorrowedBook(@PathVariable("book-id") Integer bookId, Authentication connectedUser) {
         return ResponseEntity.ok(service.approveReturnBorrowedBook(bookId, connectedUser));
     }
-
-
 }
