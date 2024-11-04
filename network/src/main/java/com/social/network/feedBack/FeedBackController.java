fee4c8b7de55318bd1cddf3963df18ceaf74b921
Mohitpatel021 <mohitpa021@gmail.com>
2024-11-04 23:52:48 +0530

    Working on the Feedback part

--- network/src/main/java/com/social/network/feedBack/FeedBackController.java
+++ network/src/main/java/com/social/network/feedBack/FeedBackController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeedBackController {
 
     private final FeedbackService service;
+
     @PostMapping()
     public ResponseEntity<?>saveFeedBack(@Valid @RequestBody FeedBackRequest request, Authentication connectedUser){
         return ResponseEntity.ok(service.save(request, connectedUser));
